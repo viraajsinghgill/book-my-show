eb6b56403b2ac9aefd287cf7f07ab4c1c14edb9f
Viraaj SIngh Gill <125380557+viraajsinghgill@users.noreply.github.com>
2023-11-06 13:14:22 +0000

    Day-03 Entertainment slider and poster slider

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -4,9 +4,9 @@ import React, { useState } from "react";
 import DefaultLayoutHoc from "../layout/Default.layout";
 
 //Components
-import EntertainmentCard from "../components/Entertainment/EntertainmentCard.Component";
 import HeroCarousel from "../components/HeroCarousel/HeroCarousel.Component";
 import PosterSlider from "../components/PosterSlider/PosterSlider.Component";
+import EntertainmentCardSlider from "../components/Entertainment/EntertainmentCard.Component";
 
 const HomePage = () => {
   const [recommendedMovies, setRecommendedMovies] = useState([]);
@@ -21,13 +21,13 @@ const HomePage = () => {
         <h1 className="text-2xl font-bold text-gray-800 sm:ml-3 ml-0 my-3">
           The best of Entertainment
         </h1>
-        <EntertainmentCard />
+        <EntertainmentCardSlider />
       </div>
 
       <div className="container mx-auto px-4 md:px-12 my-8">
         <PosterSlider
           title="Recommended Movies"
-          subject="List of recommended movies"
+          subtitle="List of recommended movies"
           posters={recommendedMovies}
           isDark={false}
         />
@@ -37,14 +37,14 @@ const HomePage = () => {
         <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
           <div className="hidden md:flex">
             <img
-            //   src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/RuPay.svg/1200px-RuPay.svg.png"
+              //   src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/RuPay.svg/1200px-RuPay.svg.png"
               alt="Rupay"
               className="w-full h-full"
             />
           </div>
           <PosterSlider
             title="Premiers"
-            subject="Brand new releases every friday"
+            subtitle="Brand new releases every friday"
             posters={premierMovies}
             isDark={true}
           />
@@ -53,7 +53,7 @@ const HomePage = () => {
       <div className="container mx-auto px-4 md:px-12 my-8">
         <PosterSlider
           title="Online Streaming Events"
-          subject="Online Stream Events"
+          subtitle="Online Stream Events"
           posters={onlineStreamEvents}
           isDark={false}
         />
